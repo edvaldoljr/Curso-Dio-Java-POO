2b18d68afed78797affc901e5491877362056174
edvaldoljr <edvaldoluizj@gmail.com>
2021-07-15 08:31:34 -0300

    Refazendo exercicio proposto aula 2

--- src/aula2/construtores/exercico/Carro.java
+++ src/aula2/construtores/exercico/Carro.java
@@ -2,19 +2,10 @@ package aula2.construtores.exercico;
 
 public class Carro {
 
-    String marca(String marca) {
+    public Carro(String marca, String modelo, int ano) {
         this.marca = marca;
-        return marca;
-    }
-
-    String modelo(String modelo) {
         this.modelo = modelo;
-        return modelo;
-    }
-
-    Integer ano(Integer ano) {
         this.ano = ano;
-        return ano;
     }
 
     private String marca;
@@ -38,19 +29,7 @@ public class Carro {
         return ano;
     }
 
-    public void setMarca(String marca) {
-        this.marca = marca;
-    }
-
-    public void setModelo(String modelo) {
-        this.modelo = modelo;
-    }
-
     public void setVariante(String variante) {
         this.variante = variante;
-    }
-
-    public void setAno(int ano) {
-        this.ano = ano;
     }
 }
--- src/aula2/construtores/exercico/IntanciaCarro.java
+++ src/aula2/construtores/exercico/IntanciaCarro.java
@@ -1,16 +1,12 @@
 package aula2.construtores.exercico;
 
-
 public class IntanciaCarro {
     public static void main(String[] args) {
-        Carro carro = new Carro ();
-        carro.setMarca("Honda");
-        carro.setModelo("Civic");
-        carro.setAno(2021);
-        carro.setVariante("8v");
+        Carro carro = new Carro ("Honda","Civic",2021);
         System.out.println(carro.getMarca());
         System.out.println(carro.getModelo());
         System.out.println(carro.getAno());
+        carro.setVariante("8v");
         System.out.println(carro.getVariante());
     }
 }
