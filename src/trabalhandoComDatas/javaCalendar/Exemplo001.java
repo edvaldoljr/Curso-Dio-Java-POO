039f6b47ace4d2674c4746b49f4ccf5ce6b01e1c
edvaldoljr <edvaldoluizj@gmail.com>
2021-08-24 02:22:41 -0300

    Exemplo 001 utilização da classe Calenda

---
+++ src/trabalhandoComDatas/javaCalendar/Exemplo001.java
@@ -0,0 +1,15 @@
+package trabalhandoComDatas.javaCalendar;
+
+import java.util.Calendar;
+
+/**
+ *  Exemplo de utilização da classe Calendar
+ */
+public class Exemplo001 {
+    public static void main(String[] args) {
+
+        Calendar agora = Calendar.getInstance();
+
+        System.out.println(agora);
+    }
+}
